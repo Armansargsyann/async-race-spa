ab8825c29d04970dadf262edd3b632e27026f2ca
Armansargsyann <armansargsyan1249@gmail.com>
2026-07-15 13:46:53 +0400

    feat: add type for reusable CarCard

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,10 @@ export interface CarIconProps {
   color: string;
   className?: string;
 }
+
+export interface CarCardProps {
+  car: Car;
+  position?: number;
+  onSelect: (id: number) => void;
+  onRemove: (id: number) => void;
+}
