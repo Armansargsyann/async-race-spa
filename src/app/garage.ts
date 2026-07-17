51f4118bc792c88eb137054681a9bc23bf4b883d
Armansargsyann <armansargsyan1249@gmail.com>
2026-07-17 12:35:26 +0400

    refactor: fix bug in GarageService

--- src/app/garage.ts
+++ src/app/garage.ts
@@ -10,14 +10,13 @@ export const GarageService = {
       throw error;
     }
   },
-};
-
-export const CreateCar = async (name: string, color: string) => {
-  try {
-    const response = await axiosInstance.post("/garage", { name, color });
-    return response.data;
-  } catch (error) {
-    console.error("Error creating car:", error);
-    throw error;
-  }
+  CreateCar: async (name: string, color: string) => {
+    try {
+      const response = await axiosInstance.post('/garage', { name, color });
+      return response.data;
+    } catch (error) {
+      console.error("Error creating car:", error);
+      throw error;
+    }
+  },
 };
